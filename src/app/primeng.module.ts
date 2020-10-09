a9c12c677acb7221ac2bd13d9d8129ae651c4750
Andreson Souza <andresonjazz@gmail.com>
2020-10-09 02:41:13 -0300

    feat: crie o componente header

--- src/app/primeng.module.ts
+++ src/app/primeng.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
+import { ToolbarModule } from 'primeng/toolbar';
+import { ButtonModule } from 'primeng/button';
+import { SplitButtonModule } from 'primeng/splitbutton';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
+  imports: [],
+  exports: [
+    CommonModule,
+    ToolbarModule,
+    ButtonModule,
+    SplitButtonModule
   ]
 })
 export class PrimengModule { }
