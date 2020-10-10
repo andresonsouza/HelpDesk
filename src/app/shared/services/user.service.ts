df4f9a1681e22e9a6ac99b8c3271b476e19137ee
Andreson Souza <andresonjazz@gmail.com>
2020-10-09 23:53:06 -0300

    feat: crie o metodo para criar ou alterar um usuario

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -2,6 +2,7 @@ import { HELP_DESK_API } from './../helpdesk.api';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { User } from '../models/user.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,16 @@ export class UserService {
 
   constructor(private http: HttpClient) { }
 
-  login(user: User) {
-    return this.http.post(`${HELP_DESK_API}/api/auth`, user);
+  login(user: User): Observable<User[]> {
+    return this.http.post<User[]>(`${HELP_DESK_API}/api/auth`, user);
+  }
+
+  createOrUpdate(user: User): Observable<User[]> {
+    if (user.id !== null && user.id !== '') {
+      return this.http.put<User[]>(`${HELP_DESK_API}/api/user`, user);
+    } else {
+      user.id = null;
+      return this.http.post<User[]>(`${HELP_DESK_API}/api/user`, user);
+    }
   }
 }
