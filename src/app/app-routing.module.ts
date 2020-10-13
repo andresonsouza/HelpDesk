655f819890b92000bb9de4abf8c8a4eae773bd88
Andreson Souza <andresonjazz@gmail.com>
2020-10-12 23:43:24 -0300

    feat: corrija a rota para o home-component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/security/login/login.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [
-  { path: '' , component: LoginComponent },
+const ROUTES: Routes = [
+  {path: '', component: HomeComponent},
+  { path: 'login' , component: LoginComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(ROUTES)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
